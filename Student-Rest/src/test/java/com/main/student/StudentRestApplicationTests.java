6a42a935b2347b7b67e7469c0cb3795837790f7d
yuvan060 <yuvansankar060@gmail.com>
2023-02-24 10:00:18 +0530

    Portal Day 3 Tasks

---
+++ Student-Rest/src/test/java/com/main/student/StudentRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
